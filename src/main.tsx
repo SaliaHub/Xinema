313d4be819db13c309d7d2256b47cca0f2f4253a
SaliaCop <2339015@claurendeau.qc.ca>
2025-02-03 08:56:06 -0500

    Ajout d'un système de routage pour facilement passer d'une page à une autre. Avec aussi des enfants d'une route qui permettent de ne charger que le contenu dont on a besoin et non toute page lorsque cela n'est pas nécessaire

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import App from "./Components/App.tsx"
+import NotFoundPage from "./NotFoundPage.tsx";
+import ShowMovies from "./Components/Home/Body/MultipleChoice/ShowMovies.tsx";
+import ShowSeries from "./Components/Home/Body/MultipleChoice/ShowSeries.tsx";
+// import ShowAll from "./Components/Home/Body/MultipleChoice/ShowAll.tsx";
+
+const Router = createBrowserRouter([
+    {
+        path: "/Xinema/",
+        element:<App/>,
+        errorElement:<NotFoundPage/>,
+        children: [
+            {
+                path: "movies",
+                element:<ShowMovies/>
+            },
+            {
+              path:"series",
+              element:<ShowSeries/>
+            }
+        ]
+    }
+]);
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={Router}/>
     </StrictMode>,
 )
